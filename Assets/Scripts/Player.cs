8283a36ac6062c1068ff9d8414d7fd4263d54142
Sandip <rockersandip96@gmail.com>
2018-02-21 23:52:50 +0200

    First project commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+
+
+	[SerializeField]
+	private float movementSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		myRigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		HandleMovement (horizontal);
+	}
+
+	//handles the movement of player
+	private void HandleMovement(float horizontal)
+	{
+		myRigidbody.velocity = new Vector2 (horizontal*movementSpeed, myRigidbody.velocity.y);
+	}
+}
