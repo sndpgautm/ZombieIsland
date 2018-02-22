ca6cc78d49e49e3890523a55ee2e65b4bf3f2ef6
Sandip <rockersandip96@gmail.com>
2018-02-22 07:15:43 +0200

    Added jumping functionality for the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,22 @@ public class Player : MonoBehaviour {
 
 	private Rigidbody2D myRigidbody;
 	private Animator myAnimator;
-
-
+	//can be edited from the inspector window
 	[SerializeField]
 	private float walkingSpeed;
-
 	private bool facingRight;
-
+	[SerializeField]
+	private Transform[] groundPoints;
+	[SerializeField]
+	private float groundRadius;
+	[SerializeField]
+	private LayerMask whatIsGround;
+	private bool isGrounded;
+	private bool jump;
+	[SerializeField]
+	private bool airControl;
+	[SerializeField]
+	private float jumpForce;
 
 	// Use this for initialization
 	void Start () {
@@ -23,18 +32,56 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	void Update(){
+		HandleInput ();
+	}
+
+
+
+
 	void FixedUpdate () 
 	{
 		float horizontal = Input.GetAxis ("Horizontal");
+		isGrounded = IsGrounded ();
 		HandleMovement (horizontal);
 		Flip (horizontal);
+		HandleLayers ();
+		ResetValues ();
 	}
 
 	//handles the movement of player
 	private void HandleMovement(float horizontal)
 	{
-		myRigidbody.velocity = new Vector2 (horizontal*walkingSpeed, myRigidbody.velocity.y);
-		myAnimator.SetFloat ("speed", Mathf.Abs(horizontal)); //speed is case sensitive and horizontal is made positive to compare
+		//checks if the player is falling toward s ground
+		if (myRigidbody.velocity.y < 0)
+		{
+			myAnimator.SetBool ("land", true);
+		}
+		//can only move if the player is on ground and has air control
+		if(isGrounded || airControl){
+			//moves the player
+			myRigidbody.velocity = new Vector2 (horizontal*walkingSpeed, myRigidbody.velocity.y);
+			myAnimator.SetFloat ("speed", Mathf.Abs(horizontal)); //speed is case sensitive and horizontal is made positive to compare
+
+		}
+
+
+		//used to jump the player
+		if (isGrounded && jump)
+		{
+			isGrounded = false;
+			myRigidbody.AddForce (new Vector2 (0, jumpForce));
+			myAnimator.SetTrigger ("jump");
+		}
+	}
+
+	//Handles the input keyss
+	private void HandleInput()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			jump = true;
+		}
 	}
 
 
@@ -49,4 +96,45 @@ public class Player : MonoBehaviour {
 			transform.localScale = theScale; //changed the value of scale in Transform
 		}
 	}
+
+
+	//Resets the values of jump, attack
+	private void ResetValues()
+	{
+		jump = false;
+	}
+
+	//Checks if the player is standing on the ground
+	private bool IsGrounded()
+	{
+		if (myRigidbody.velocity.y <= 0)
+		{
+			foreach (Transform point in groundPoints)
+			{
+				Collider2D[] colliders = Physics2D.OverlapCircleAll (point.position, groundRadius, whatIsGround);
+
+				for (int i = 0; i < colliders.Length; i++)
+				{
+					if (colliders [i].gameObject != gameObject) 
+					{	
+						myAnimator.ResetTrigger ("jump");
+						myAnimator.SetBool ("land", false);
+						return true;
+					}
+				}
+			}	
+		}
+		return false;
+	}
+		
+	//Handles the animator layers
+	private void HandleLayers()
+	{
+		if (!isGrounded) {
+			myAnimator.SetLayerWeight (1, 1); //Layer weight is set to 1 and Layer number 1 refers to AirLayer
+		}else{
+			myAnimator.SetLayerWeight (1, 0); //0 refers to GroundLayer
+		}
+	}
+
 }
