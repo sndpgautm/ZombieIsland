1b05774153aca7f1ee52c0d134f2b7bdfbf0a73a
utsabk <utsab.kharel@metropolia.fi>
2018-03-12 11:10:49 +0200

    Spikes and Zombies added in SunSet Scene

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+	private bool playerInZone;
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		playerInZone = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (/*Input.GetKeyDown (KeyCode.RightShift) && */playerInZone) {
+			Application.LoadLevel (levelToLoad);
+		}
+		
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.name == "Player")
+			playerInZone = true;
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.name == "Player")
+			playerInZone = false;
+	}
+}
